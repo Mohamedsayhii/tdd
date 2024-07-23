408221068393c0608a270f4ef74d3d40ec3b9cc8
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-07-23 17:58:37 +0100

    Add tests to capitalize function

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,8 @@
 const capitalize = (str) => {
+	const isNotLetter = /^[0-9\W]+$/.test(str);
+	if (typeof str === 'number' || isNotLetter)
+		return `Can capitalize letters only`;
+
 	return [str[0].toUpperCase(), str.slice(1)].join('');
 };
 
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,8 +1,25 @@
 import capitalize from './capitalize';
 
-test('capitalize', () => {
-	const input = 'hama';
-	const actual = capitalize(input);
-	const expected = input.replace(input[0], input[0].toUpperCase());
-	expect(actual).toMatch(expected);
+test('capitalize first character', () => {
+	expect(capitalize('hama')).toMatch('Hama');
+});
+
+test('capitalize first character', () => {
+	expect(capitalize('moha sayhi')).toMatch('Moha sayhi');
+});
+
+test('should reject an input of type number', () => {
+	expect(capitalize(1234)).toBe(`Can capitalize letters only`);
+});
+
+test('should reject a numbers only string', () => {
+	expect(capitalize('1234')).toBe(`Can capitalize letters only`);
+});
+
+test('should reject a symbols only string', () => {
+	expect(capitalize('!!!@@##$%^&*^')).toBe(`Can capitalize letters only`);
+});
+
+test('should reject symbols and numbers string', () => {
+	expect(capitalize('!!!@@##12345^')).toBe(`Can capitalize letters only`);
 });
